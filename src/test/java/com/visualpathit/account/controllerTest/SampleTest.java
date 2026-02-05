bbf4736d5cd51817fe2f456ee698fa83ac9a96ef
SaajanDevops <bc230412452ros@vu.edu.pk>
2026-02-05 20:20:43 +0500

    Code tested fixed the issue

--- src/test/java/com/visualpathit/account/controllerTest/SampleTest.java
+++ src/test/java/com/visualpathit/account/controllerTest/SampleTest.java
@@ -1,13 +1,12 @@
 package com.visualpathit.account.controllerTest;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class SampleTest {
-	@Test
-	public void SampleTestHappyFlow(){
-		assertEquals("Hello".length(), 5);
-	}
 
+    @Test
+    public void SampleTestHappyFlow() {
+        assertEquals(5, "Hello".length());
+    }
 }
